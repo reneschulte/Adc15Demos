5a30d57f565a20765817d5cfb3cef85d47b01b4b
Rene Schulte <contact@rene-schulte.info>
2015-09-23 11:15:41 +0200

    Initial commit of WP DirectX Band sample

---
+++ WpDxBandSample/WpDxBandSample/Helpers/Smoothen.cs
@@ -0,0 +1,31 @@
+﻿namespace WpDxBandSample.Helpers
+{
+    public class Smoothen
+    {
+        private readonly double[] _inputs;
+
+        public Smoothen()
+            : this(5)
+        {
+        }
+
+        public Smoothen(int factor)
+        {
+            _inputs = new double[factor];
+        }
+
+        public double GetValue(double input)
+        {
+            var sum = input;
+
+            for (var index = _inputs.Length - 2; index >= 0; index--)
+            {
+                sum += _inputs[index];
+                _inputs[index + 1] = _inputs[index];
+            }
+
+            _inputs[0] = input;
+            return sum / _inputs.Length;
+        }
+    }
+}
