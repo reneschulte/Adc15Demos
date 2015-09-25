51eb3b796b9a9ed78fd1ca63922b24116baac5f4
Rene Schulte <contact@rene-schulte.info>
2015-09-25 17:42:39 +0200

    Initial checkin of the basic unity demo

---
+++ Unity Demos/BasicUnityDemo/Assets/Scripts/CannonBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CannonBehaviour : MonoBehaviour
+{
+    private CardboardHead _head;
+
+    public float ForceMagnitude = 300f;
+
+    void Start()
+    {
+        _head = Camera.main.GetComponent<StereoController>().Head;
+    }
+
+    void Update()
+    {
+        if (Cardboard.SDK.Triggered)
+        {
+            var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+            var rigidBody = sphere.AddComponent<Rigidbody>();
+            rigidBody.mass = 0.1f;
+            rigidBody.position = _head.transform.position;
+            rigidBody.AddForce(_head.Gaze.direction * ForceMagnitude);
+        }
+    }
+}
---
+++ Unity Demos/BasicUnityDemo/Assets/Scripts/RotateBehavior.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class RotateBehavior : MonoBehaviour
+{
+    public float Speed = 2;
+
+	void Update ()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * Speed, Space.World);
+    }
+}
