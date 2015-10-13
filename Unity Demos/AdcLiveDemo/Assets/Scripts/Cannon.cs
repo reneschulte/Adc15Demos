3007accc08b054c34a27bfbbb81fd9ae803a6366
Rene Schulte <contact@rene-schulte.info>
2015-10-13 15:36:52 +0200

    Added Unity AdcLiveDemo

---
+++ Unity Demos/AdcLiveDemo/Assets/Scripts/Cannon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour
+{
+    public float ForceMagnitude = 300f;
+
+    void Update()
+    {
+        if (Cardboard.SDK.Triggered)
+        {
+            var sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            sphere.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+
+            var rigidBody = sphere.AddComponent<Rigidbody>();
+            rigidBody.mass = 0.2f;
+            rigidBody.position = transform.position;
+            rigidBody.AddForce(transform.forward * ForceMagnitude);
+        }
+    }
+}
---
+++ Unity Demos/AdcLiveDemo/Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+    public float Speed = 2;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, Time.deltaTime * Speed, Space.World);
+    }
+}
